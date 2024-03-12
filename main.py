ce49e7b3600b0b0b5fbdc44ad4fea516ea869bed
Pipe281 <felipeic@hotmail.cl>
2024-03-12 20:25:24 -0300

    Cambios 12/03/24

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
 from flask import Flask, jsonify, request
+#from test import 
 app = Flask(__name__)
+
+from prueba import users_list
+
+@app.route('/')
+def hello():
+    return jsonify({"message": "Hello!"})
+
+
+@app.route("/user")
+def getProducts():
+    return users_list
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=4000)
+
+
+
 
---
+++ prueba.py
@@ -0,0 +1,5 @@
+users_list = [
+            {"name":"Felipe", "surname": "Carrasco", "url": "http://felipe.carrasco.cl", "age":"33"},
+            {"name":"Javier", "surname": "Godoy", "url": "http://cojines.cl", "age":"23"},
+            {"name":"Tamara", "surname": "Valenzuela", "url": "http://tvalenzuela.com", "age":"43"}
+            ]
