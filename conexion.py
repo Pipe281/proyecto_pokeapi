84e15ff1ab08d4d70fde961a3b55a0ccb28f00f4
Pipe281 <felipeic@hotmail.cl>
2024-03-13 18:24:33 -0300

    Endpoint Pokemon

--- conexion.py
+++ conexion.py
@@ -1,13 +1,44 @@
 import mysql.connector
 
-# Establece los parámetros de conexión
-conexion = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="root",
-    database="dbpoke"
-)
+class conexion_bd:
+    def __init__(self, host, user, password, database):
+        self.host=host
+        self.user=user
+        self.password=password
+        self.database=database
+        self.connect=None
 
+    def conectar(self):
+        self.connect=mysql.connector.connect(
+            host=self.host,
+            user=self.user,
+            password=self.password,
+            database=self.database
+        )
+
+    def desconectar(self):
+        if self.connect:
+            self.connect.close()
+
+    def query_select(self, consulta):
+        cursor=self.connect.cursor()
+        cursor.execute(consulta)
+        return cursor.fetchall()
+
+def main():
+    conexion= conexion_bd("localhost","root", "root", "dbpoke")
+    try:
+        conexion.conectar()
+        query="SELECT * FROM pokemon"
+        resultado=conexion.query_select(query)
+        print(resultado)
+    except mysql.connector.Error as error: 
+        print("Error al conectarse a la base de datos", error)
+    finally:
+        conexion.desconectar()
+
+main()
+''' 
 # Verifica si la conexión fue exitosa
 if conexion.is_connected():
     print("Conexión exitosa a la base de datos")
@@ -29,3 +60,4 @@ if conexion.is_connected():
     
 else:
     print("Error al conectar a la base de datos")
+'''
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from flask import Flask, jsonify, request
+from conexion import conexion_bd
+import mysql.connector
 #from test import 
 app = Flask(__name__)
 
@@ -9,10 +11,20 @@ def hello():
     return jsonify({"message": "Hello!"})
 
 
-@app.route("/user")
-def getProducts():
-    return users_list
+@app.route("/pokemon")
+def getPokemon():
+    conexion= conexion_bd("localhost","root", "root", "dbpoke")
+    try:
+        conexion.conectar()
+        query="SELECT * FROM pokemon"
+        resultado=conexion.query_select(query)
+        print(resultado)
+    except mysql.connector.Error as error: 
+        print("Error al conectarse a la base de datos", error)
+    finally:
+        conexion.desconectar()
 
+    return resultado
 
 if __name__ == '__main__':
     app.run(debug=True, port=4000)
